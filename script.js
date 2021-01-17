da3ab919cc8a85b8951b5b243ed49ba0668335aa
ilchak <ilchak12@gmail.com>
2021-01-17 21:59:30 +0200

    added tabs js, rename menu.js to script.js

---
+++ script.js
@@ -0,0 +1,30 @@
+//menu mobile
+let mobileMenu = document.querySelector(".menu-burger");
+let mainMenu = document.querySelector(".menu");
+
+mobileMenu.addEventListener("click", function() {
+    mobileMenu.classList.toggle("active-menu");
+    if(mobileMenu.classList.contains("active-menu")) {
+        mainMenu.classList.add("active-menu");
+    } else {
+        mainMenu.classList.remove("active-menu");
+    }
+})
+
+//tabs
+
+let tabItems = document.querySelectorAll(".tab-title-item");
+let tabContent = document.querySelectorAll(".tab-content");
+
+for(let i = 0; i < tabItems.length; i++) {
+    tabItems[i].addEventListener("click", function() {
+        let activeItem = document.querySelector(".tab-title-item.active");
+        let activeContent = document.querySelector(".tab-content.active");
+
+        activeItem.classList.remove("active");
+        tabItems[i].classList.add("active");
+
+        activeContent.classList.remove("active");
+        tabContent[i].classList.add("active");
+    })
+}
